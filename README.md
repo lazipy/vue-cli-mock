08bbde7c16db897f8b3cd404bfea4d4c1931df93
李子屏 <877286986@qq.com>
2019-07-26 11:40:51 +0800

    提交

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # vue-cli-mock
-A quick build mock plug-in based on vue-cli3
+
+#### 安装
+
+```
+npm install vue-cli-mock -S
+```
+
+#### 使用
+
+vue.config.js
+```
+const mock = require('vue-cli-mock')
+
+module.exports = {
+  devServer: {
+    before (app) {
+      mock(app)
+    }
+  }
+}
+```
+
+根目录下 mock/user.js
+
+```
+module.exports = {
+  'GET /user/login': (req, res) => {
+    res.json({ custom: '1111' })
+  },
+  '/user/1': (req, res) => {
+    res.json({ custom: 'response' })
+  },
+  '/dicts': {
+    status: [1, 2, 3]
+  }
+}
+```
---
+++ dist/index.amd.js
@@ -0,0 +1,37 @@
+define(function () { 'use strict';
+
+  const requireContext = require('require-context');
+  const mockFiles = requireContext(`${__dirname}/mock`, true, /\.js$/);
+
+  let mockModules = [];
+  if (mockFiles.keys().length > 0) {
+    mockFiles.keys().forEach(key => {
+      mockModules.push(require(`./mock/${key}`));
+    });
+  }
+
+  module.exports = function (app) {
+    const methods = ['GET', 'POST', 'PUT', 'DELETE', 'get', 'post', 'put', 'delete'];
+    for (let mock of mockModules) {
+      Object.keys(mock).forEach(key => {
+        let method = key.split(' ')[0];
+        let url = key.split(' ')[1];
+        if (!methods.includes(method)) {
+          method = 'get';
+          url = key;
+        } else {
+          method = method.toLowerCase();
+        }
+
+        app[method](url, function (req, res) {
+          if (typeof mock[key] === 'function') {
+            mock[key](req, res);
+          } else {
+            res.json(mock[key]);
+          }
+        });
+      });
+    }
+  };
+
+});
